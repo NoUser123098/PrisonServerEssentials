174088a66a8a9312b7b2a6b595174939c04b728d
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-09-18 08:45:35 -0500

    Update main.php

--- src/NoUser123098/PrisonServerEssentials/main.php
+++ src/NoUser123098/PrisonServerEssentials/main.php
@@ -12,8 +12,21 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Server;
 use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\permission\DefaultPermissions;
+use pocketmine\utils\UUID;
 
-class Main extends PluginBase{
-    
+class main extends PluginBase{
+    private $config;
+    private $permission;
+    const NOT_FOUND = null;
+    const INVALID_NAME = -1;
+    const ALREADY_EXISTS = 0;
+    const SUCCESS = 1;
+    private $loadGroups = false;
+    private $msg;
+    private $provider;
+    private $userDateMgr;
+    private $attachments = [], $groups = [], $defaultPermissions = [];
 }
 ?>
